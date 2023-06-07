18bbe43c5fe36e5ff49695cddab3b45429db9ea1
jeromehaas <hello@jeromehaas.ch>
2023-06-07 17:18:46 +0200

    Setup: input-field component

---
+++ website/components/01-atoms/input-field/input-field.jsx
@@ -0,0 +1,39 @@
+import Paragraph from 'components/01-atoms/paragraph/paragraph';
+import PropTypes from 'prop-types';
+
+const InputField = ({ className, id, label, placeholder, type, register, validation, isRequired, errorText, errors }) => {
+
+	return (
+		<div className={ `${ className } input-field ${ errors && errors[id] ? 'input-field--error' : null }` }>
+			<label className="input-field__label" htmlFor={ id }>{ `${ label }${ isRequired ? '*' : '' }` }</label>
+			<input className="input-field__input" type={ type } placeholder={ placeholder } { ...(register ? register(id, validation) : {}) } />
+			{ errors && errors[id] && errorText ? <Paragraph className="input-field__error paragraph--small">{ errorText }</Paragraph> : null }
+		</div>
+	);
+
+};
+
+InputField.propTypes = {
+	className: PropTypes.string,
+	id: PropTypes.string.isRequired,
+	type: PropTypes.string.isRequired,
+	label: PropTypes.string.isRequired,
+	isRequired: PropTypes.bool,
+	placeholder: PropTypes.string.isRequired,
+	register: PropTypes.func.isRequired,
+	errorText: PropTypes.string,
+	errors: PropTypes.shape({
+		id: PropTypes.shape({
+			message: PropTypes.string,
+		}),
+	}),
+};
+
+InputField.defaultProps = {
+	className: '',
+	isRequired: false,
+	errorText: '',
+	errors: [],
+};
+
+export default InputField;
