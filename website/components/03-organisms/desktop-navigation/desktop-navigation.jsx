955b6018c73b1ef5a980ac60581357e17a32a01c
jeromehaas <hello@jeromehaas.ch>
2023-06-09 17:19:01 +0200

    Setup: desktop-navigation component

---
+++ website/components/03-organisms/desktop-navigation/desktop-navigation.jsx
@@ -0,0 +1,24 @@
+import Anchor from 'components/01-atoms/anchor/anchor';
+import Section from 'components/04-layouts/section/section';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const DesktopNavigation = () => {
+
+	return (
+		<Section className="desktop-navigation">
+			<Link className="desktop-navigation__logo-link" href="/">
+				<Image className="desktop-navigation__logo" src="/logos/logo-wespi-sins-bw-text-only.svg" width="300" height="300" alt="WESPI SINS Papeterie- und Bluemeparadies" />
+			</Link>
+			<div className="desktop-navigation__links links">
+				<Anchor className="links__item" href="/">Papeterie</Anchor>
+				<Anchor className="links__item" href="/">Blueme</Anchor>
+				<Anchor className="links__item" href="/">Über Uns</Anchor>
+				<Anchor className="links__item" href="/">Kontakt</Anchor>
+			</div>
+		</Section>
+	);
+
+};
+
+export default DesktopNavigation;
--- website/components/04-layouts/website/website.jsx
+++ website/components/04-layouts/website/website.jsx
@@ -1,11 +1,13 @@
 import PropTypes from 'prop-types';
 import Footer from 'components/03-organisms/footer/footer';
 import OpeningTimes from 'components/03-organisms/opening-times/opening-times';
+import DesktopNavigation from 'components/03-organisms/desktop-navigation/desktop-navigation';
 
 const Website = ({ className, children }) => {
 
 	return (
 		<div className={ `${ className } website` }>
+			<DesktopNavigation />
 			{ children }
 			<OpeningTimes />
 			<Footer />
