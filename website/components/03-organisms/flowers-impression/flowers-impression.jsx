953805869108c7e301535475d5f7908d914f367d
jeromehaas <hello@jeromehaas.ch>
2023-06-15 15:19:58 +0200

    Setup: flowers-impression component

---
+++ website/components/03-organisms/flowers-impression/flowers-impression.jsx
@@ -0,0 +1,14 @@
+import Section from 'components/04-layouts/section/section';
+import Picture from 'components/01-atoms/picture/picture';
+
+const FlowersImpression = () => {
+
+	return (
+		<Section className="flowers-impression">
+			<Picture className="flowers-impression__image" src="/images/placeholder.png" width={ 1920 } height={ 1600 } />
+		</Section>
+	);
+
+};
+
+export default FlowersImpression;
