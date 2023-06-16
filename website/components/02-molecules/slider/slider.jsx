26bbd0b1cd746285bfbd116a2c1b226a3005499c
jeromehaas <hello@jeromehaas.ch>
2023-06-16 22:45:16 +0200

    Setup: slider component

---
+++ website/components/02-molecules/slider/slider.jsx
@@ -0,0 +1,47 @@
+import Picture from 'components/01-atoms/picture/picture';
+import PropTypes from 'prop-types';
+import { gsap } from 'gsap';
+import { useRef, useEffect } from 'react';
+
+const Slider = ({ className = '', sliderController = null }) => {
+
+	// SETUP REFS
+	const sliderRef = useRef();
+	const sliderTimelineRef = useRef();
+
+	// UPDATE SLIDER ORDER
+	useEffect(() => {
+		gsap.context(() => {
+			sliderTimelineRef.current = gsap.timeline();
+			if (sliderController.viewportWidth && sliderController.viewportWidth >= 950) {
+				sliderTimelineRef.current.to(`.slider__item:nth-child(${ sliderController.counter })`, { left: '0%' }, 0);
+				sliderTimelineRef.current.to(`.slider__item:nth-child(-n+${ sliderController.counter })`, { left: '0%' }, 0);
+				sliderController.counter !== sliderController.images.length && sliderTimelineRef.current.to(`.slider__item:nth-child(${ sliderController.counter + 1 })`, { left: 'calc(-100% - 120px)' }, 0);
+				sliderController.counter !== sliderController.images.length && sliderTimelineRef.current.to(`.slider__item:nth-child(n+${ sliderController.counter + 2 })`, { left: 'calc(-200% - 120px)' }, 0);
+			}
+			if (sliderController.viewportWidth && sliderController.viewportWidth < 950) {
+				sliderTimelineRef.current.to(`.slider__item:nth-child(${ sliderController.counter })`, { right: '0%' }, 0);
+				sliderTimelineRef.current.to(`.slider__item:nth-child(-n+${ sliderController.counter })`, { right: '0%' }, 0);
+				sliderController.counter !== sliderController.images.length && sliderTimelineRef.current.to(`.slider__item:nth-child(${ sliderController.counter + 1 })`, { right: 'calc(-100% - 80px)' }, 0);
+				sliderController.counter !== sliderController.images.length && sliderTimelineRef.current.to(`.slider__item:nth-child(n+${ sliderController.counter + 2 })`, { right: 'calc(-200% - 80px)' }, 0);
+			}
+		}, sliderRef);
+	}, [sliderController.counter]);
+
+	return (
+		<div className={ `${ className } slider ` } onTouchStart={ sliderController.handleTouchStart } onTouchMove={ sliderController.handleTouchMove } onTouchEnd={ sliderController.handleTouchEnd } ref={ sliderRef }>
+			{ sliderController.images.map((image) => {
+				return (
+					<Picture className="slider__item" src={ image.src } key={ image.id } alt={ image.alt } />
+				);
+			})}
+		</div>
+	);
+
+};
+
+Slider.propTypes = {
+	className: PropTypes.string,
+};
+
+export default Slider;
