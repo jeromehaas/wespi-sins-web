0c93d814b7f38824a1cbbde7555a2e31bf4150c6
jeromehaas <hello@jeromehaas.ch>
2023-06-17 13:58:52 +0200

    Setup: about-us-speciality component

---
+++ website/components/03-organisms/about-us-speciality/about-us-speciality.jsx
@@ -0,0 +1,14 @@
+import Section from 'components/04-layouts/section/section';
+import Heading from 'components/01-atoms/heading/heading';
+
+const AboutUsSpeciality = () => {
+
+	return (
+		<Section className="about-us-speciality">
+			<Heading className="about-us-speciality__text" level="h4">Wir sind ein eingespieltes Team und freuen uns über   berührende Begegnungen mit verschiedensten Menschen. Ihr Wunsch oder Anliegen trifft auf unsere Kreativität – gemeinsam finden wir für Sie das passende Produkt.</Heading>
+		</Section>
+	);
+
+};
+
+export default AboutUsSpeciality;
