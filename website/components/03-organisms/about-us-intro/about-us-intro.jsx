be947b925851c23be8619b5db52d8dec00c18bf5
jeromehaas <hello@jeromehaas.ch>
2023-06-17 13:44:03 +0200

    Setup: about-us-intro component

---
+++ website/components/03-organisms/about-us-intro/about-us-intro.jsx
@@ -0,0 +1,16 @@
+import Section from 'components/04-layouts/section/section';
+import Heading from 'components/01-atoms/heading/heading';
+
+const AboutUsIntro = () => {
+
+	return (
+		<Section className="about-us-intro">
+			<Heading className="about-us-intro__heading" level="h1">
+				Im kleinen Team <br /> überzeugen wir gross.
+			</Heading>
+		</Section>
+	);
+
+};
+
+export default AboutUsIntro;
