b115230fd41ba3b77ac3c6cbec90d205683a495e
jeromehaas <hello@jeromehaas.ch>
2023-06-19 09:39:19 +0200

    Setup: contact-info component

---
+++ website/components/03-organisms/contact-info/contact-info.jsx
@@ -0,0 +1,35 @@
+import Section from 'components/04-layouts/section/section';
+import Paragraph from 'components/01-atoms/paragraph/paragraph';
+import Heading from 'components/01-atoms/heading/heading';
+import Link from 'next/link';
+
+const ContactInfo = () => {
+
+	return (
+		<Section className="contact-info">
+			<div className="contact-info__intro intro">
+				<Paragraph className="intro__text">Uns ist es eine Ehre, in den alten, seit 1881 standhaften Bahnhofsmauern kreativ zu wirken und den Menschen mit Blumen und Papeterieartikeln ein Lächeln ins Gesicht zu zaubern. Wir schätzen den persönlichen Kontakt sehr und freuen uns, Sie zu sehen, zu hören oder von Ihnen zu lesen.</Paragraph>
+			</div>
+			<div className="contact-info__contact-details contact-details">
+				<Heading className="contact-details__heading" level="h2">Wespi Sins</Heading>
+				<Paragraph className="contact-details__item">Papeterie- und Bluemeparadies</Paragraph>
+				<Paragraph className="contact-details__item">Bahnhofstrasse 15</Paragraph>
+				<Paragraph className="contact-details__item">5643 Sins</Paragraph>
+				<Link className="contact-details__item paragraph" href="tel:0417870887">041 787 08 87</Link>
+				<Link className="contact-details__item paragraph" href="mailto:info@wespi-sins.ch">info@wespi-sins.ch</Link>
+			</div>
+			<div className="contact-info__business-hours business-hours">
+				<Heading className="business-hours__heading" level="h2">Öffnungszeiten</Heading>
+				<Paragraph className="business-hours__item">Montag, Dienstag, Donnerstag und Freitag</Paragraph>
+				<Paragraph className="business-hours__item">08.30 – 12.00, 14.00 – 18.30 Uhr</Paragraph>
+				<Paragraph className="business-hours__item">Samstag</Paragraph>
+				<Paragraph className="business-hours__item">08.00 – 15.00 Uhr</Paragraph>
+
+			</div>
+
+		</Section>
+	);
+
+};
+
+export default ContactInfo;
