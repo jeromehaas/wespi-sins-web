954c6f202b20dd7db8c37eeb514626781daf5bf2
jeromehaas <hello@jeromehaas.ch>
2023-06-19 08:59:46 +0200

    Setup: contact-intro component

---
+++ website/components/03-organisms/contact-intro/contact-intro.jsx
@@ -0,0 +1,16 @@
+import Section from 'components/04-layouts/section/section';
+import Heading from 'components/01-atoms/heading/heading';
+
+const ContactIntro = () => {
+
+	return (
+		<Section className="contact-intro">
+			<Heading className="contact-intro__heading" level="h1">
+				Wir sind gerne für Sie da und freuen uns auf Ihren Besuch.
+			</Heading>
+		</Section>
+	);
+
+};
+
+export default ContactIntro;
