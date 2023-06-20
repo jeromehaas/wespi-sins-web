caa2ed824ff84e853fe95c2753e0c278014793a5
jeromehaas <hello@jeromehaas.ch>
2023-06-20 11:42:48 +0200

    Setup: data-privacy-intro component

---
+++ website/components/03-organisms/data-privacy-intro/data-privacy-intro.jsx
@@ -0,0 +1,16 @@
+import Section from 'components/04-layouts/section/section';
+import Heading from 'components/01-atoms/heading/heading';
+
+const DataPrivacyIntro = () => {
+
+	return (
+		<Section className="data-privacy-intro">
+			<Heading className="data-privacy-intro__heading" level="h1">
+				Datenschutz
+			</Heading>
+		</Section>
+	);
+
+};
+
+export default DataPrivacyIntro;
