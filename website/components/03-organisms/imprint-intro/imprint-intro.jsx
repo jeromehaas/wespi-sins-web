2037e4252c94809fe203532d1aa88a7d0e442a57
jeromehaas <hello@jeromehaas.ch>
2023-06-20 10:57:05 +0200

    Setup: imprint-intro component

---
+++ website/components/03-organisms/imprint-intro/imprint-intro.jsx
@@ -0,0 +1,16 @@
+import Section from 'components/04-layouts/section/section';
+import Heading from 'components/01-atoms/heading/heading';
+
+const ImprintIntro = () => {
+
+	return (
+		<Section className="imprint-intro">
+			<Heading className="imprint-intro__heading" level="h1">
+				Impressum
+			</Heading>
+		</Section>
+	);
+
+};
+
+export default ImprintIntro;
