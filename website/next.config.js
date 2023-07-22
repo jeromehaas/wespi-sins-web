dedf81e376092d49dc2a1889951366dac4f60260
jeromehaas <hello@jeromehaas.ch>
2023-07-22 21:49:23 +0200

    Refactor: remove i18 settings

--- website/next.config.js
+++ website/next.config.js
@@ -2,10 +2,6 @@ module.exports = {
 	images: {
 		domains: ['localhost', 'picsum.photos', 'wespi-sins-web-development.fra1.digitaloceanspaces.com', 'wespi-sins-web-production.fra1.digitaloceanspaces.com'],
 	},
-	i18n: {
-		locales: ['de-ch'],
-		defaultLocale: 'de-ch',
-	},
 	eslint: {
 		ignoreDuringBuilds: true,
 	},
