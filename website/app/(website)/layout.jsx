0bc35a25496d3ff3f37b885ac563e414f3a650a8
jeromehaas <hello@jeromehaas.ch>
2023-07-24 10:56:09 +0200

    Setup: base metadata in layout component

--- website/app/(website)/layout.jsx
+++ website/app/(website)/layout.jsx
@@ -2,6 +2,21 @@
 import Website from 'components/04-layouts/website/website';
 import { Fragment } from 'react';
 
+// META DESCRIPTION
+const metadata = {
+	title: 'Papeterie- und Bluemeparadies Wespi',
+	description: 'Seit Jahren mit viel Herzblut und Leidenschaft für Sie da. Ob im Alltag, an Hochzeiten, zu Trauer- oder Geburtstagsfesten – wir erfüllen saisonale Blumenwünsche. Zu Blumen gehört oft eine Karte und dazu braucht es natürlich einen toller Stift.',
+	openGraph: {
+		title: 'Papeterie- und Bluemeparadies Wespi',
+		description: 'Seit Jahren mit viel Herzblut und Leidenschaft für Sie da. Ob im Alltag, an Hochzeiten, zu Trauer- oder Geburtstagsfesten – wir erfüllen saisonale Blumenwünsche. Zu Blumen gehört oft eine Karte und dazu braucht es natürlich einen toller Stift.',
+		url: 'https://wespi-sins.ch',
+		siteName: 'Next.js',
+		images: [{ url: 'https://cms.wespi-sins.ch/admin/70674f63fc3904c20de0.svg', width: 800, height: 600 }],
+		locale: 'de-ch',
+		type: 'website',
+	},
+};
+
 // LAYOUT
 const RootLayout = ({ children }) => {
 
@@ -17,4 +32,5 @@ const RootLayout = ({ children }) => {
 };
 
 // EXPORT
+export { metadata };
 export default RootLayout;
