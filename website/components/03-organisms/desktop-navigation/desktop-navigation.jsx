15c21b7ef0b858ef1e39509063f270e98c655b5c
jeromehaas <hello@jeromehaas.ch>
2023-08-27 15:36:33 +0200

    Refactor: highlight current links in desktop-navigation

--- website/components/03-organisms/desktop-navigation/desktop-navigation.jsx
+++ website/components/03-organisms/desktop-navigation/desktop-navigation.jsx
@@ -1,9 +1,10 @@
 'use client';
 
 // IMPORTS
-import { useRef, useContext } from 'react';
+import { useRef, useContext, useEffect } from 'react';
 import { OrderContext } from 'contexts/order-context';
 import { usePathname, useRouter } from 'next/navigation';
+import { gsap } from 'gsap';
 import Anchor from 'components/01-atoms/anchor/anchor';
 import Section from 'components/04-layouts/section/section';
 import Image from 'next/image';
@@ -38,10 +39,10 @@ const DesktopNavigation = () => {
 						<Image className="bar__logo" src="/logos/logo-wespi-sins-bw-text-only.svg" width="300" height="300" alt="WESPI SINS Papeterie- und Bluemeparadies" priority />
 					</Link>
 					<div className="bar__links links">
-						<Anchor className="links__item" href="/stationery">Papeterie</Anchor>
-						<Anchor className="links__item" href="/flowers">Blueme</Anchor>
-					<Anchor className="links__item" href="/about-us">Über uns</Anchor>
-						<Anchor className="links__item" href="/contact">Kontakt</Anchor>
+						<Anchor className={ `links__item ${ pathname === '/stationery' ? 'links__item--active' : null }` } href="/stationery">Papeterie</Anchor>
+						<Anchor className={ `links__item ${ pathname === '/flowers' ? 'links__item--active' : null }` } href="/flowers">Blueme</Anchor>
+						<Anchor className={ `links__item ${ pathname === '/about-us' ? 'links__item--active' : null }` } href="/about-us">Über uns</Anchor>
+						<Anchor className={ `links__item ${ pathname === '/contact' ? 'links__item--active' : null }` } href="/contact">Kontakt</Anchor>
 					</div>
 				</div>
 			</div>
