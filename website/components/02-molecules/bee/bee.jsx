0b1a5b12818c468ede5e8551813ae84b571705ac
jeromehaas <hello@jeromehaas.ch>
2023-08-27 15:56:33 +0200

    Refactor: slow down speed of bee animation

--- website/components/02-molecules/bee/bee.jsx
+++ website/components/02-molecules/bee/bee.jsx
@@ -38,13 +38,13 @@ const Bee = ({ className = '' }) => {
 		const context = gsap.context(() => {
 			beeTimelineRef.current = gsap.timeline({ delay: 0.25, scrollTrigger: { trigger: beeRef.current, start: 'top bottom-=160px', end: 'bottom top+=160px', markers: false } });
 			if (viewportWidth >= 1200) {
-				beeTimelineRef.current.to('.bee .bee__image', { right: '120px', duration: 1 });
+				beeTimelineRef.current.to('.bee .bee__image', { right: '120px', duration: 2 });
 			} else if (viewportWidth < 1200 && viewportWidth >= 850) {
-				beeTimelineRef.current.to('.bee .bee__image', { right: '64px', duration: 1 });
+				beeTimelineRef.current.to('.bee .bee__image', { right: '64px', duration: 2 });
 			} else if (viewportWidth < 850) {
-				beeTimelineRef.current.to('.bee .bee__image', { right: '-320px', duration: 1 });
+				beeTimelineRef.current.to('.bee .bee__image', { right: '-320px', duration: 2 });
 				beeTimelineRef.current.to('.bee .bee__image', { right: '200%', duration: 0 });
-				beeTimelineRef.current.to('.bee .bee__image', { right: 'calc(100% - 160px)', duration: 1 });
+				beeTimelineRef.current.to('.bee .bee__image', { right: 'calc(100% - 160px)', duration: 2 });
 			};
 		}, beeRef);
 		return () => { return context.revert(); };
