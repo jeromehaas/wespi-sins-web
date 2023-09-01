f9f93a9e7d787d37f94150edc90b60349427c607
jeromehaas <hello@jeromehaas.ch>
2023-09-01 16:55:40 +0200

    Refactor: change dash to arrow in slider-navigation component

--- website/components/02-molecules/slider-navigation/slider-navigation.jsx
+++ website/components/02-molecules/slider-navigation/slider-navigation.jsx
@@ -13,10 +13,10 @@ const SliderNavigation = ({ className = '', sliderController }) => {
 	useEffect(() => {
 		gsap.context(() => {
 			sliderNavigationTimelineRef.current = gsap.timeline();
-			sliderNavigationTimelineRef.current.to('.navigation__item .item__dash', { width: '0px' }, 0);
+			sliderNavigationTimelineRef.current.to('.navigation__item .item__arrow', { width: '0' }, 0);
 			sliderNavigationTimelineRef.current.to('.navigation__item .item__index', { color: '#9E453E' }, 0);
 			sliderNavigationTimelineRef.current.to(`.navigation__item:nth-child(${ sliderController.counter }) .item__index`, { color: '#000000' }, 0);
-			sliderController.counter !== sliderController.images.length && sliderNavigationTimelineRef.current.to(`.navigation__item:nth-child(${ sliderController.counter }) .item__dash`, { width: '24px' }, 0);
+			sliderController.counter !== sliderController.images.length && sliderNavigationTimelineRef.current.to(`.navigation__item:nth-child(${ sliderController.counter }) .item__arrow`, { width: '24px' }, 0);
 		}, sliderNavigationRef);
 	}, [sliderController.counter]);
 
@@ -26,7 +26,9 @@ const SliderNavigation = ({ className = '', sliderController }) => {
 				return (
 					<div className="navigation__item item" key={ image.id }>
 						<Paragraph className="item__index" onClick={ () => { return sliderController.handleJump(index + 1); } }>{ String(index + 1).padStart(2, 0) }</Paragraph>
-						<figure className="item__dash" />
+						<svg className="item__arrow" width="30" height="13" viewBox="0 0 30 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path d="M24 11.125L28.5 6.5L24 1.875M1.5 6.5L28.5 6.5L1.5 6.5Z" stroke="#9E453E" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+						</svg>
 					</div>
 				);
 			})}
