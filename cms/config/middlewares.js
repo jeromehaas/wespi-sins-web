85dd0032522f8a3b367c416b1e5f89595eb5287a
jeromehaas <hello@jeromehaas.ch>
2023-09-17 20:39:11 +0200

    Fix: add correct paddings for intro components

--- cms/config/middlewares.js
+++ cms/config/middlewares.js
@@ -8,8 +8,8 @@ module.exports = ({ env }) => [
 				useDefaults: true,
 				directives: {
 					'connect-src': ['\'self\'', 'https:'],
-					'img-src': ['\'self\'', 'data:', 'blob:', `${ env('SPACE_BUCKET')}.${ env('SPACE_ENDPOINT')}`],
-					'media-src': ['\'self\'', 'data:', 'blob:', `${ env('SPACE_BUCKET')}.${ env('SPACE_ENDPOINT')}`],
+					'img-src': ['\'self\'', 'data:', 'blob:', 'strapi.io', `${ env('SPACE_BUCKET')}.${ env('SPACE_ENDPOINT')}`],
+					'media-src': ['\'self\'', 'data:', 'blob:', 'strapi.io', `${ env('SPACE_BUCKET')}.${ env('SPACE_ENDPOINT')}`],
 					upgradeInsecureRequests: null,
 				},
 			},
